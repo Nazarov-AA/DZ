a4533b6887bc125340dc382466b024d2f5014182
alex <nazarchick1@gmail.com>
2020-09-25 15:01:47 +0300

    add animation in MainActivity5.kt

--- app/src/main/java/com/example/dz/MainActivity5.kt
+++ app/src/main/java/com/example/dz/MainActivity5.kt
@@ -1,11 +1,21 @@
 package com.example.dz
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main5.*
 
 class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main5)
+
+
+        cat.setBackgroundResource(R.drawable.cats)
+
+        val myCat: AnimationDrawable = cat.background as AnimationDrawable
+
+        myCat.start()
+
     }
 }
